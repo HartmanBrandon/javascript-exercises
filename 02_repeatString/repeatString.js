422294d123d9a93ea7b0f27829634f0e9ea1e7ae
HartmanBrandon <brandon@jokerbusinesssolutions.com>
2024-03-02 20:04:52 -0500

    Created a string and number placeholders for the function call. Created a for loop to print string call 3 times

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,7 +1,12 @@
 // Writing this comment to test a github push
-const repeatString = function() {
+const repeatString = function () {};
 
-};
+let stringPlaceHolder;
+let numPlaceHolder;
 
+
+for ( let i = 0; i < numPlaceHolder; i++{
+    console.log(stringPlaceHolder);
+})
 // Do not edit below this line
 module.exports = repeatString;
