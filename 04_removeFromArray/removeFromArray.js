37dc3b5e3b548128f64a778782a9b13db13af416
HartmanBrandon <brandon@jokerbusinesssolutions.com>
2024-03-02 22:05:46 -0500

    Created a function that creates an array and removes an item

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,10 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, removedItem) {
 
+let returnedArr = [];
+let index = arr.indexOf(removedItem);
+return returnedArr.splice(index);
+
+    // This is where I will remove from array
 };
 
 // Do not edit below this line
