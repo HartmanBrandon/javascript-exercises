a67829c0aaf16994c76625e5103fe7eee1029f1e
HartmanBrandon <brandon@jokerbusinesssolutions.com>
2024-03-02 20:47:08 -0500

    Deleted unused code

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,6 @@
-const reverseString = function(word) {
-let getWord = word.split("").reverse().join("");
-return getWord;
+const reverseString = function (word) {
+  let getWord = word.split("").reverse().join("");
+  return getWord;
 };
 
 // Do not edit below this line
