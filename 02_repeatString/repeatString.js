677e42acb3a8cb9025e29ff72f219ea8111409da
HartmanBrandon <brandon@jokerbusinesssolutions.com>
2024-03-02 20:25:27 -0500

    Made s for loop to print heyheyhey once passed a word and number

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,12 +1,13 @@
 // Writing this comment to test a github push
-const repeatString = function () {};
+const repeatString = function (word, num) {
+  
+  let stringPlaceHolder = "";
+  for (let i = 0; i < num; i++) {
+    stringPlaceHolder += word;
+  }
+  return stringPlaceHolder;
 
-let stringPlaceHolder;
-let numPlaceHolder;
+};
 
-
-for ( let i = 0; i < numPlaceHolder; i++{
-    console.log(stringPlaceHolder);
-})
 // Do not edit below this line
 module.exports = repeatString;
