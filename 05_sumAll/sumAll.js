0d10c9bef249c5e369ca9a24d12389b8d7cd11ab
HartmanBrandon <brandon@jokerbusinesssolutions.com>
2024-03-02 22:40:05 -0500

    Created a function that takes 2 integers and returns th sum of every number between

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,11 @@
-const sumAll = function() {
+const sumAll = function(startingNum, endingNum) {
 
+let finalSum = 0;
+
+for (let item = startingNum; item <= endingNum; item++){
+    finalSum = item + finalSum;
+}
+return finalSum;
 };
 
 // Do not edit below this line
